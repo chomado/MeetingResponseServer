a9d877b42f2ebe8f9253898503201d11da34953d
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2019-05-24 22:44:58 +0900

    日にち指定できるように＆外部のクラスに分けた

---
+++ MeetingResponseServer/GetMeeting.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingResponseServer
+{
+    public static class MeetingInfo
+    {
+        public static async Task<IActionResult> GetMeeting(DateTime startTime, DateTime endTime)
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+
+            // Even if this is a console application here, a daemon application is a confidential client application
+            IConfidentialClientApplication app;
+
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+
+            AuthenticationResult result = null;
+            result = await app.AcquireTokenForClient(scopes)
+                .ExecuteAsync();
+
+            var httpClient = new HttpClient();
+            var apiCaller = new ProtectedApiCallHelper(httpClient);
+            var response = await apiCaller.CallWebApiAndProcessResultASync($"https://graph.microsoft.com/v1.0/users/{config.MyUserId}/calendarview?startdatetime={startTime}&enddatetime={endTime}", result.AccessToken);
+            return new OkObjectResult(response);
+        }
+    }
+}
--- MeetingResponseServer/GetMeetingInfo.cs
+++ MeetingResponseServer/GetMeetingInfo.cs
@@ -19,28 +19,7 @@ namespace MeetingResponseServer
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
-
-            // Even if this is a console application here, a daemon application is a confidential client application
-            IConfidentialClientApplication app;
-
-            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
-                .WithClientSecret(config.ClientSecret)
-                .WithAuthority(new Uri(config.Authority))
-                .Build();
-
-            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
-            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
-            // a tenant administrator
-            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
-
-            AuthenticationResult result = null;
-            result = await app.AcquireTokenForClient(scopes)
-                .ExecuteAsync();
-
-            var httpClient = new HttpClient();
-            var apiCaller = new ProtectedApiCallHelper(httpClient);
-            var response = await apiCaller.CallWebApiAndProcessResultASync($"https://graph.microsoft.com/v1.0/users/{config.MyUserId}/calendarview?startdatetime=2019-05-23T15:00:00.000Z&enddatetime=2019-05-24T14:59:59.999Z", result.AccessToken);
+            var response = await MeetingInfo.GetMeeting(startTime: DateTime.UtcNow, endTime: DateTime.UtcNow.AddDays(1));
             return new OkObjectResult(response);
         }
     }
